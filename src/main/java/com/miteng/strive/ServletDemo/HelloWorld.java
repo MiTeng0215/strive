503e9d1c4c22a321dc14718f7da7a6daf176c918
苗腾 <miteng0215@163.com>
2021-07-07 22:02:42 +0800

    2020.7.7 23:00

--- src/main/java/com/miteng/strive/ServletDemo/HelloWorld.java
+++ src/main/java/com/miteng/strive/ServletDemo/HelloWorld.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloWorld {
+
     @ResponseBody
     @RequestMapping("hello")
     public String hello(HttpServletRequest request, HttpServletResponse response) {
@@ -19,8 +22,10 @@ public class HelloWorld {
     @ResponseBody
     @RequestMapping("login")
     public String login(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("请求收到了");
         Cookie cookie = new Cookie("miao", "teng");
         response.addCookie(cookie);
+        System.out.println("目标方法执行了");
         return "请先登录";
     }
 }
